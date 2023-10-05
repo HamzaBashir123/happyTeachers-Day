cc29f4f3781eafa11ecc98a88022cde533fcacd8
hamzabashir1268@gmail.com <hamzabashir1268@gmail.com>
2023-10-05 16:33:27 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+    <div className="background"></div>
+    <div className="content">
+      <h1>Happy Teacher's Day!</h1>
+      <p><span>Sir Sufiyan</span> <br />Your guidance and wisdom inspire us every day. Thank you for being an amazing teacher!</p>
+    </div>
+  </div>
+  )
+}
+
+export default App
